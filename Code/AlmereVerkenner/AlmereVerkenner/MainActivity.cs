6e6e2324355e991f04d6218a963efe260defb2ce
John Doe <gamefake@live.nl>
2016-09-07 17:30:49 +0200

    Added QR scanner

---
+++ Code/AlmereVerkenner/AlmereVerkenner/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware;
+using Android.Graphics;
+using System.IO;
+using ZXing.Mobile;
+
+namespace AlmereVerkenner
+{
+    [Activity(Label = "Almere Verkenner", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Android.Hardware.Camera camera;
+        private Button captureButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set view to home screen
+            SetContentView(Resource.Layout.Main);
+
+            // Set the camer view
+            //textureView = this.FindViewById<TextureView>(Resource.Id.cameraView);
+            //textureView.SurfaceTextureListener = this;
+
+            // Add the capture button event
+            captureButton = this.FindViewById<Button>(Resource.Id.captureButton);
+            captureButton.Click += CaptureButton_Click;
+
+            // Initialize scanner (for android only)
+            MobileBarcodeScanner.Initialize(Application);
+        }
+
+        private async void CaptureButton_Click(object sender, EventArgs e)
+        {
+            // Capture picture
+            MobileBarcodeScanner scanner = new MobileBarcodeScanner();
+            scanner.UseCustomOverlay = false;
+            scanner.TopText = "Hold the camera up to the barcode\nAbout 6 inches away";
+            scanner.BottomText = "Wait for the barcode to automatically scan!";
+            object result = await scanner.Scan();
+            
+            // Read QR code
+            
+            
+            // Check QR result with current GPS position
+            
+            
+            // Add point if OK
+        }
+    }
+}
+
---
+++ Code/AlmereVerkenner/AlmereVerkenner/README.txt
@@ -0,0 +1,31 @@
+﻿Almere Verkenner
+
+
+TODO:
+- Login screen
+	- Login boxes
+- Home screen
+	- Camera view
+	- Capture button
+	- Menu button
+- Shop screen
+	- Total points
+	- Shop elements (prizes)
+- Settings screen
+	- Version
+- Menu screen
+	- Go to home
+	- Go to shop
+	- Go to settings
+	- Account info
+- QR Generator/Decoder
+	- Encode GPS locations
+
+Versions:
+v0.1
+- Features
+	- ...
+- Fixes
+	- ...
+- Issues
+	- ...
